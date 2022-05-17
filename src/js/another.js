71f159c59fe76e4da27831aca46a8ca2711bb2e3
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-05-17 21:37:20 +0200

    fix

--- src/js/another.js
+++ src/js/another.js
@@ -1,7 +1,6 @@
 export function mov(firstRegister, secondRegister) {
   if (firstRegister.innerHTML != 0) {
     secondRegister.innerHTML = firstRegister.innerHTML;
-    firstRegister.innerHTML = 0;
   }
 }
 export function xchg(firstRegister, secondRegister) {
